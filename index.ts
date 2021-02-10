ab90601a48bcdf03d617306128afe42518b04e88
BuildTools <unconfigured@null.spigotmc.org>
2021-02-09 22:33:56 -0500

    using pug view engine and express static

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
 // Imports
-
 import * as express from 'express';
-const socket = require('socket.io');
-
+require('pug')
 const api = require('./routes/api');
-
-
 // Initializations 
 const app = express();
-
-
+const http = require('http').Server(app);
+const socket = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost"
+  }
+});
+const server = http.listen(3000, function() {
+  console.log("Socket.Io on *:3000");
+});
 // Express Middlewares
-
-app.use(express.json())
-app.use('/api', api)
-app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'pug')
+app.use(express.json());
+app.use('/api', api);
+app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
-  
+  res.render('index', {
+    colors: require('colors')
+  })
 })
+
 app.listen(80)
+console.log('Running');
--- routes/api.ts
+++ routes/api.ts
@@ -1,7 +1,8 @@
+import * as express from 'express'
 import { Router as r } from 'express'
 var Router = r()
 Router.get('/join', (req, res) => {
   
 })
-
+Router.use(express.static('./assets'))
 export = Router;
