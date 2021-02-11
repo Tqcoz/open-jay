6811ae922232e234c5e52f561d51a39395c0e5a1
BuildTools <unconfigured@null.spigotmc.org>
2021-02-10 20:01:41 -0500

    prototype register and beginning of login

--- database/User.ts
+++ database/User.ts
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose')
 const model = mongoose.model('User', new mongoose.Schema({
-    id: Number,
+    id: String,
     username: String,
     discriminator: Number,
     avatar: String,
     friends: Array,
+    email: String,
     guilds: Array,
     password: String
 }))
 export = {
-    getUser: async function (id: Number) {
+    getUser: async function (id: String) {
         let User = await model.findOne({id}).catch(()=>{
             return;
         });
         User.friends = []
-        User.friends.forEach(async (friendId: { "": Number; }) => {
+        User.friends.forEach(async (friendId: { "": String; }) => {
             User.friends.push(await model.findOne({id: friendId}))
         });
         return User;
--- routes/api.ts
+++ routes/api.ts
@@ -1,17 +1,53 @@
 import * as express from 'express'
+import * as crypto from 'crypto'
+import * as bcrypt from 'bcrypt'
 import { Router as r } from 'express'
+const User = require('../database/User')
 var Router = r();
+const salt = "$2b$10$EJQ31SrNKhVIYQ5I/SORI.";
 Router.use(express.json())
 Router.use(express.urlencoded({extended: false}))
-Router.get('/join', (req, res) => {
-  
+Router.post('/join', async (req, res) => {
+  // Prototype, we're going to be making this more secure soon
+  let tryUser = await User.model.findOne({ email: req.body.email }).catch(() => { })
+  if (tryUser) {
+    res.send({
+      status: 401,
+      error: 'User with Email already exists'
+    })
+  } else {
+    User.model.create({
+      id: await crypto.randomBytes(16).toString('hex'),
+      username: req.body.username,
+      email: req.body.email,
+      password: (await bcrypt.hash(req.body.password, salt)).toString(),
+      discriminator: await crypto.randomInt(9999).toString().padStart(4, '0')
+    })
+  }
 })
-Router.post('/login', (req, res) => {
+Router.post('/login', async (req, res) => {
+  let tryUser = await User.model.findOne({ email: req.body.email }).catch(() => { });
+  if (!tryUser) {
+    res.send({
+      status: 401,
+      error: 'Could not fetch User with the details'
+    })
+  } else {
+    console.log('test');
+    console.log(await bcrypt.compare(req.body.password, tryUser.password));
+    
+    if (await bcrypt.compare(req.body.password, tryUser.password)) {
+      console.log(tryUser);
+      
+    } else {
+      res.send({
+        status: 401,
+        error: 'Could not fetch User with the details'
+      })
+    }
+  } 
   console.log(req.body);
-  res.json({
-    status: 401,
-    error: 'Invalid Email/Password'
-  })
+  
   
 })
 Router.use(express.static('./assets'))
